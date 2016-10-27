7d8aa1fd2d294ee017b9c99327add66c7f13f3bc
Iain Reid <iainreid382@gmail.com>
2016-10-27 22:41:07 +0100

    [chore] Initialised GH pages

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+'use strict'
+
+// Dependencies
+const glob = require('glob')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+// Hack for Ubuntu on Windows: interface enumeration fails with EINVAL, so return empty.
+try {
+  require('os').networkInterfaces()
+} catch (e) {
+  require('os').networkInterfaces = () => ({})
+}
+
+module.exports = {
+  entry: [
+    './node_modules/angular',
+    ...glob.sync('./source/**/*.js').sort((a, b) => a.length > b.length)
+  ],
+  output: {
+    path: 'build',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'source/index.html'
+    })
+  ],
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  watchOptions: {
+    poll: true
+  }
+}
